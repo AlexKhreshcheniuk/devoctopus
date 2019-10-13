e66c894e7eeb9ba9f0a1cc03890cad4ee12f0b81
Valdemar Kostenko <vladimir.kostenko@playtech.com>
2019-10-13 20:52:20 +0300

    DO-013: [WIP] modify webpack.config.dev to handle background-image in css

--- configs/webpack.config.dev.js
+++ configs/webpack.config.dev.js
@@ -18,6 +18,16 @@ module.exports = merge(common, {
         test: /\.(sass|scss)$/,
         use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]',
+          },
+        }],
+      },
     ],
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
