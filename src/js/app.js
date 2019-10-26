84174b4b970459d6e97cfd743eaacd91aaf36515
Alex Khreshcheniuk <hellooalekS@gmail.com>
2019-10-26 16:32:06 +0300

    DO-025: Added hanling of send Contact us form

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,41 @@
-function includeHTML() {
+async function includeHTML() {
   let i;
   let element;
   let file;
-  let xhttp;
+  let length;
 
   /* Loop through a collection of all HTML elements: */
   const z = document.querySelectorAll('[do-include-html]');
-  for (i = 0; i < z.length; i += 1) {
+  length = z.length;
+  for (i = 0; i < length; i += 1) {
     element = z[i];
     /* search for elements with a certain attribute: */
     file = element.getAttribute('do-include-html');
     if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      // eslint-disable-next-line no-loop-func
-      xhttp.onreadystatechange = function () {
-        if (this.readyState === 4) {
-          if (this.status === 200) { element.innerHTML = this.responseText; }
-          if (this.status === 404) { element.innerHTML = 'Page not found.'; }
-          /* Remove the attribute, and call this function once more: */
-          element.removeAttribute('do-include-html');
-          includeHTML();
-        }
-      };
-      xhttp.open('GET', file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
+      let response = await fetch(file);
+      if(length - 1 == i) {
+        // TODO: find approach to execute script inside loaded file
+        addHandler()
+      }
+      if (response.ok) {
+        element.innerHTML= await response.text();
+      } else {
+        element.innerHTML = 'Page not found.';
+      }
     }
   }
 }
+
+function addHandler() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailjs.sendForm('gmail', 'template_asI2nKbM', this).then(function(response) {
+          document.getElementById('contact-us-title').textContent = 'We will reach out to you shortly'
+          document.getElementById('contact-form').style.display = 'none';
+       }, function(error) {
+          console.log('FAILED...', error);
+       });;
+    });
+  }
 
 includeHTML();
