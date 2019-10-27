3ce1370c191686a272c771b166ed5976df692a33
Alex Khreshcheniuk <hellooalekS@gmail.com>
2019-10-27 22:11:05 +0200

    DO-029: Deployment issue

--- configs/webpack.config.prod.js
+++ configs/webpack.config.prod.js
@@ -1,6 +1,7 @@
 const cssnano = require('cssnano');
 const merge = require('webpack-merge');
 
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
@@ -27,6 +28,9 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(['public'], {
+      root: process.cwd(),
+  }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css',
