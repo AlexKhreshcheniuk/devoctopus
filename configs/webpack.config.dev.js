841fd695c64cf30645586b319b0b97c34baa7976
Serhii Poturnak <N/A>
2020-01-22 17:02:30 +0200

    Use interpolation and image compress

--- configs/webpack.config.dev.js
+++ configs/webpack.config.dev.js
@@ -1,32 +1,22 @@
-const path = require('path');
-const glob = require('glob');
-
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const common = require('./webpack.config.common.js');
 
-const generateHTMLPlugins = () => glob.sync('./src/**/*.html').map(
-  (dir) => new HTMLWebpackPlugin({
-    filename: path.basename(dir), // Output
-    template: dir, // Input
-  }),
-);
-
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'source-map',
-  devServer: {
-    contentBase: 'src',
-    watchContentBase: true,
-    hot: true,
-    open: true,
-    port: process.env.PORT || 3000,
-    host: process.env.HOST || 'localhost',
+module.exports = merge(
+  common('development'),
+  {
+    devtool: 'source-map',
+    devServer: {
+      contentBase: 'src',
+      watchContentBase: true,
+      hot: true,
+      open: true,
+      port: process.env.PORT || 3000,
+      host: process.env.HOST || 'localhost',
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+    ],
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    ...generateHTMLPlugins(),
-  ],
-});
+);
--- configs/webpack.config.prod.js
+++ configs/webpack.config.prod.js
@@ -2,38 +2,24 @@ const cssnano = require('cssnano');
 const merge = require('webpack-merge');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const common = require('./webpack.config.common.js');
 
-module.exports = merge(common, {
-  mode: 'production',
-  optimization: {
-    minimize: true,
+module.exports = merge(
+  common('production'),
+  {
+    optimization: {
+      minimize: true,
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessor: cssnano,
+        cssProcessorOptions: { discardComments: { removeAll: true } },
+        canPrint: true,
+      }),
+    ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
-    }),
-    new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /\.css$/g,
-      cssProcessor: cssnano,
-      cssProcessorOptions: { discardComments: { removeAll: true } },
-      canPrint: true,
-    }),
-  ],
-});
+);
